6e8935beb61a4d4611dbfabc6182eb2c17ee30b4
tanawat.jtl <tanawat.jtl@gmail.com>
2020-11-03 00:08:55 +0700

    update home

--- REST/wwwroot/JavaScript/Layout/_Order.js
+++ REST/wwwroot/JavaScript/Layout/_Order.js
@@ -1,12 +1,1 @@
-﻿$(document).ready(function () {
-    screenScroll()
-});
-
-function screenScroll() {
-    var height_P = $(window).height() - $('.navbar').height()
-    $('[scroll]').slimScroll({
-        height: height_P,
-        railBorderRadius: 0,
-        railVisible: true
-    });
-}
+﻿
